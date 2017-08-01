9f6cdf8262d9ca03ba8318081c59616c0c0465f5
Andrew Leitsius <leits@quintagroup.com>
2017-08-01 18:27:38 +0300

    CleanUp roles

--- openregistry/assets/basic/models.py
+++ openregistry/assets/basic/models.py
@@ -4,7 +4,6 @@ from zope.interface import implementer
 
 from openregistry.assets.core.models import (
     IAsset, Asset as BaseAsset,
-    view_role, create_role, edit_role, Administrator_role
 )
 
 
