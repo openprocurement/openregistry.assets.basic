4ec5ccefdd82c1021566d93a664ab939e638503c
ivanka-jakimchuk <ivanka-jakimchuk@quintagroup.org>
2017-08-04 12:32:32 +0300

    Add files (#5)
    * Add files
    
    * Update docs.cfg
    
    * Update docs.py
    
    * Update conf.py
    
    * Update docs.cfg
    
    * Update conf.py

---
+++ docs.py
@@ -0,0 +1,68 @@
+
+# -*- coding: utf-8 -*-
+import json
+import os
+from datetime import timedelta, datetime
+from uuid import uuid4
+
+from openregistry.api.models import get_now
+from openregistry.assets.basic.tests.base import PrefixedRequestClass, test_asset_data, BaseAssetWebTest
+from webtest import TestApp
+
+now = datetime.now()
+
+test_asset_data = test_asset_data.copy()
+
+
+class DumpsTestAppwebtest(TestApp):
+    def do_request(self, req, status=None, expect_errors=None):
+        req.headers.environ["HTTP_HOST"] = "api-sandbox.ea.openprocurement.org"
+        if hasattr(self, 'file_obj') and not self.file_obj.closed:
+            self.file_obj.write(req.as_bytes(True))
+            self.file_obj.write("\n")
+            if req.body:
+                try:
+                    self.file_obj.write(
+                        '\n' + json.dumps(json.loads(req.body), indent=2, ensure_ascii=False).encode('utf8'))
+                    self.file_obj.write("\n")
+                except:
+                    pass
+            self.file_obj.write("\n")
+        resp = super(DumpsTestAppwebtest, self).do_request(req, status=status, expect_errors=expect_errors)
+        if hasattr(self, 'file_obj') and not self.file_obj.closed:
+            headers = [(n.title(), v)
+                       for n, v in resp.headerlist
+                       if n.lower() != 'content-length']
+            headers.sort()
+            self.file_obj.write(str('\n%s\n%s\n') % (
+                resp.status,
+                str('\n').join([str('%s: %s') % (n, v) for n, v in headers]),
+            ))
+
+            if resp.testbody:
+                try:
+                    self.file_obj.write('\n' + json.dumps(json.loads(resp.testbody), indent=2, ensure_ascii=False).encode('utf8'))
+                except:
+                    pass
+            self.file_obj.write("\n\n")
+        return resp
+
+
+class AuctionResourceTest(BaseAuctionWebTest):
+    initial_data = test_asset_data
+    docservice = True
+
+    def setUp(self):
+        self.app = DumpsTestAppwebtest(
+            "config:tests.ini", relative_to=os.path.dirname(base_test.__file__))
+        self.app.RequestClass = PrefixedRequestClass
+        self.app.authorization = ('Basic', ('broker', ''))
+        self.couchdb_server = self.app.app.registry.couchdb_server
+        self.db = self.app.app.registry.db
+        if self.docservice:
+            self.setUpDS()
+            self.app.app.registry.docservice_url = 'http://public.docs-sandbox.ea.openprocurement.org'
+            
+    def test_docs_acceleration(self):
+        # SANDBOX_MODE=TRUE
+        pass
