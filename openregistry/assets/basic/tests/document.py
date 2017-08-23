85a0fc3678522e7ff1144435dccf0224c6844411
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-08-23 17:14:27 +0300

    Remove duplicate test blanks for document

--- openregistry/assets/basic/tests/document.py
+++ openregistry/assets/basic/tests/document.py
@@ -1,22 +1,9 @@
 # -*- coding: utf-8 -*-
-import os
 import unittest
 
-from openregistry.api.tests.base import snitch
 from openregistry.api.tests.blanks.mixins import ResourceDocumentTestMixin
 from openregistry.assets.basic.tests.base import (
     AssetContentWebTest
-)
-from openregistry.assets.basic.tests.document_blanks import (
-    # AssetDocumentWithDSResourceTest
-    not_found,
-    create_document_in_active_asset_status,
-    put_asset_document_invalid,
-    patch_asset_document,
-    create_asset_document_error,
-    create_asset_document_json_invalid,
-    create_asset_document_json,
-    put_asset_document_json
 )
 
 
