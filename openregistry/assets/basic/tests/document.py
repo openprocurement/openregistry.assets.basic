72e46fd09b7b339a9bdc48ae46e990c7d5103963
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-08-23 17:00:21 +0300

    Use ResourceDocumentTestMixin

--- openregistry/assets/basic/tests/document.py
+++ openregistry/assets/basic/tests/document.py
@@ -3,6 +3,7 @@ import os
 import unittest
 
 from openregistry.api.tests.base import snitch
+from openregistry.api.tests.blanks.mixins import ResourceDocumentTestMixin
 from openregistry.assets.basic.tests.base import (
     AssetContentWebTest
 )
@@ -19,21 +20,11 @@ from openregistry.assets.basic.tests.document_blanks import (
 )
 
 
-class AssetDocumentWithDSResourceTest(AssetContentWebTest):
+class AssetDocumentWithDSResourceTest(AssetContentWebTest, ResourceDocumentTestMixin):
     docservice = True
 
     # status, in which operations with asset documents (adding, updating) are forbidden
     forbidden_document_modification_actions_status = 'active'
-
-    test_create_asset_document_json = snitch(create_asset_document_json)
-    test_put_asset_document_json = snitch(put_asset_document_json)
-    test_patch_asset_document = snitch(patch_asset_document)
-
-    test_not_found = snitch(not_found)
-    test_create_document_active_status = snitch(create_document_in_active_asset_status)
-    test_create_asset_document_error = snitch(create_asset_document_error)
-    test_create_asset_document_json_invalid = snitch(create_asset_document_json_invalid)
-    test_put_asset_document_invalid = snitch(put_asset_document_invalid)
 
 
 def suite():
