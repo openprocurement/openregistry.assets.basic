8a1eb1973f57f8e5d3aeec8f9d68f626aa3b1943
Scandie <grizly.vl@gmail.com>
2017-09-08 14:55:23 +0300

    Move AssetResourceTest blanks to assets.core
    - Change initial_data to 'test_asset_basic_data'
    - AssetTest > AssetBasicTest; AssetResourceTest > AssetBasicResourceTest
    - Add AssetResourceTestMixin to AssetBasicResourceTest

--- openregistry/assets/basic/tests/asset.py
+++ openregistry/assets/basic/tests/asset.py
@@ -4,40 +4,33 @@ import unittest
 
 from openregistry.api.tests.base import BaseWebTest, snitch
 from openregistry.api.tests.blanks.mixins import ResourceTestMixin
+
+from openregistry.assets.core.tests.blanks.mixins import AssetResourceTestMixin
+
 from openregistry.assets.basic.tests.base import (
-    test_asset_data, BaseAssetWebTest
+    test_asset_basic_data, BaseAssetWebTest
 )
 from openregistry.assets.basic.tests.asset_blanks import (
-    # AssetResourceTest
-    patch_asset,
-    asset_concierge_patch,
-    administrator_change_delete_status,
-    administrator_change_complete_status,
     # AssetTest
     simple_add_asset
 )
 
 
-class AssetTest(BaseWebTest):
-    initial_data = test_asset_data
+class AssetBasicTest(BaseWebTest):
+    initial_data = test_asset_basic_data
     relative_to = os.path.dirname(__file__)
 
     test_simple_add_asset = snitch(simple_add_asset)
 
 
-class AssetResourceTest(BaseAssetWebTest, ResourceTestMixin):
+class AssetBasicResourceTest(BaseAssetWebTest, ResourceTestMixin, AssetResourceTestMixin):
     initial_status = 'pending'
-
-    test_08_patch_asset = snitch(patch_asset)
-    test_09_asset_concierge_patch = snitch(asset_concierge_patch)
-    test_10_administrator_change_delete_status = snitch(administrator_change_delete_status)
-    test_11_administrator_change_complete_status = snitch(administrator_change_complete_status)
 
 
 def suite():
     tests = unittest.TestSuite()
-    tests.addTest(unittest.makeSuite(AssetResourceTest))
-    tests.addTest(unittest.makeSuite(AssetTest))
+    tests.addTest(unittest.makeSuite(AssetBasicResourceTest))
+    tests.addTest(unittest.makeSuite(AssetBasicTest))
     return tests
 
 
