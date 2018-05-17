b20acc632f787548064deb21e666b739e02a0d52
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-05-17 19:43:12 +0300

    Use _internal_type

--- openregistry/assets/basic/subscribers/event_handlers.py
+++ openregistry/assets/basic/subscribers/event_handlers.py
@@ -4,7 +4,7 @@ from openregistry.assets.core.events import AssetInitializeEvent
 from openregistry.assets.core.utils import get_now
 
 
-@subscriber(AssetInitializeEvent, assetType="basic")
+@subscriber(AssetInitializeEvent, _internal_type="basic")
 def tender_init_handler(event):
     """ initialization handler for basic assets """
     event.asset.date = get_now()
