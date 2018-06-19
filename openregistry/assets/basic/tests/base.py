a6ca8c491aaf9356cdce191ef7c20d88fce6bc49
vitalii-motso <vitalii.motso@gmail.com>
2018-06-19 13:03:19 +0300

    add mock config (#44)

--- openregistry/assets/basic/tests/base.py
+++ openregistry/assets/basic/tests/base.py
@@ -4,13 +4,23 @@ from copy import deepcopy
 
 from openregistry.assets.core.tests.base import (
     BaseAssetWebTest as BaseAWT,
-    test_asset_basic_data
+    test_asset_basic_data,
+    connection_mock_config,
+    MOCK_CONFIG as BASE_MOCK_CONFIG
 )
+
+
+from openregistry.assets.basic.tests.fixtures import PARTIAL_MOCK_CONFIG
+MOCK_CONFIG = connection_mock_config(PARTIAL_MOCK_CONFIG,
+                                     base=BASE_MOCK_CONFIG,
+                                     connector=('plugins', 'api', 'plugins',
+                                                'assets.core', 'plugins'))
 
 
 class BaseAssetWebTest(BaseAWT):
     initial_auth = ('Basic', ('broker', ''))
     relative_to = os.path.dirname(__file__)
+    mock_config = MOCK_CONFIG
 
     def setUp(self):
         self.initial_data = deepcopy(test_asset_basic_data)
@@ -20,4 +30,5 @@ class BaseAssetWebTest(BaseAWT):
 class AssetContentWebTest(BaseAssetWebTest):
     init = True
     initial_status = 'pending'
+    mock_config = MOCK_CONFIG
 
---
+++ openregistry/assets/basic/tests/fixtures.py
@@ -0,0 +1,6 @@
+PARTIAL_MOCK_CONFIG = {
+    "assets.basic":{
+        "use_default":True,
+        "aliases":["basic1"]
+    }
+}
