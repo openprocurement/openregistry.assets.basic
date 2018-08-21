71bb2a0f1143d5e9ca876130280d92618d41fda6
vitalii-motso <vitalii.motso@gmail.com>
2018-08-21 17:10:25 +0300

    add default level acc

--- openregistry/assets/basic/constants.py
+++ openregistry/assets/basic/constants.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 
 DEFAULT_ASSET_BASIC_TYPE = 'basic'
+
+DEFAULT_LEVEL_OF_ACCREDITATION = {'create': [1],
+                                  'edit': [2]}
--- openregistry/assets/basic/tests/fixtures.py
+++ openregistry/assets/basic/tests/fixtures.py
@@ -1,7 +1,7 @@
 PARTIAL_MOCK_CONFIG = {
-    "assets.basic":{
-        "use_default":True,
-        "aliases":["basic1"],
+    "assets.basic": {
+        "use_default": True,
+        "aliases": ["basic1"],
         "accreditation": {
             "create": [1],
             "edit": [2]
