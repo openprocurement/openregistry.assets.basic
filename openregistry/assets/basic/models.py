7594ae3e791b6864626d4554ceeceee3d4370fe7
Bohdan Dmytriv <bdmbdsm@users.noreply.github.com>
2018-11-20 13:11:17 +0200

    Bring validate_relatedLot (#49)

--- openregistry/assets/basic/models.py
+++ openregistry/assets/basic/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from schematics.types import StringType
+from schematics.types import StringType, ValidationError
 from schematics.types.compound import ModelType, ListType
 from zope.interface import implementer
 
@@ -18,3 +18,7 @@ class Asset(BaseAsset):
     _internal_type = 'basic'
     assetType = StringType(default="basic")
     additionalClassifications = ListType(ModelType(AssetAdditionalClassification), default=list())
+
+    def validate_relatedLot(self, data, lot):
+        if data['status'] == 'active' and not lot:
+            raise ValidationError(u'This field is required.')
